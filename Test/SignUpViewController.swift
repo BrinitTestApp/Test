f68f3649ece94bb159a20507e3406f549a0df994
BrinitTestApp <brinittest@gmail.com>
2025-09-08 18:21:59 +0530

    Initial Commit

---
+++ Test/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  Test
+//
+//  Created by Brinit on 08/09/25.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
